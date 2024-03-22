4791b8ba446745e78e40b54242a1d2a224bb47cb
Phil Humpherys <160782207+phumpherys64@users.noreply.github.com>
2024-03-22 15:47:26 -0600

    ultimate

--- python/list_index.py
+++
@@ -1,9 +0,0 @@
-alist = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five']
-
-prev = None
-curr = alist[0]
-
-for nxt in alist[1:] + [None]:
-    print(f'prev: {prev}, curr: {curr}, next: {nxt}')
-    prev = curr
-    curr = nxt
