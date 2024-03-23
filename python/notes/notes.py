94b782776eccaf6b2d7bf696b7a51ca18ef383ae
Phil Humpherys <160782207+phumpherys64@users.noreply.github.com>
2024-03-23 12:54:54 -0600

    notes and docstrings

---
+++ python/notes/notes.py
@@ -0,0 +1,22 @@
+def say_hello(name):
+    """A simple function that says hello... Richie style"""
+    print(f"Hello {name}, is it me you're looking for?")
+    
+"""This is the summary line
+
+This is the further elaboration of the docstring. Within this section,
+you can elaborate further on details as appropriate for the situation.
+Notice that the summary and the elaboration is separated by a blank new
+line.
+"""
+
+# Notice the blank line above. Code should continue on this line.
+
+class SimpleClass:
+    """Class docstrings go here."""
+
+    def say_hello(self, name: str):
+        """Class method docstrings go here."""
+
+        print(f'Hello {name}')
+        
