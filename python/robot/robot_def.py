068e90a5f5cfe4cf6ff1162721a6d87a3e01fa58
Phil Humpherys <160782207+phumpherys64@users.noreply.github.com>
2024-03-23 15:27:39 -0600

    rough draft

--- python/robot/robot_def.py
+++ python/robot/robot_def.py
@@ -1,6 +1,43 @@
+"""
+robot.py
+    
+A file argument can be used i.e.:  robot.py inputfile.txt
+Or pipe stdin:  cat input.txt | robot.py
+    
+Example INSTRUCTIONS:
+UP 1
+RIGHT 10
+DOWN 5
+LEFT 2
+
+% cat input.txt | python3 robot.py
+(8,-4)    
+
+% chmod +x ./robot.py    
+% ./robot.py input.txt 
+(8,-4)
+    
+"""
+
 import fileinput
 
 def robot():
+    """returns (x,y) from input (stdin or filename as arguement) of <INSTRUCTIONS>
+    
+    Example <INSTRUCTIONS>:
+    ----------------------
+    
+    UP 1
+    RIGHT 10
+    DOWN 5
+    LEFT 2
+    
+    Returns
+    -------
+    "(x,y)" str  Coordinates of robot after executing <INSTRUCTIONS>
+    
+    """
+    
     x = 0
     y = 0
     decimals = ['0','1','2','3','4','5','6','7','8','9']
