b698ed0b70ba5bb188f887521583de29faba962d
Phil Humpherys <160782207+phumpherys64@users.noreply.github.com>
2024-03-23 15:27:39 -0600

    rough draft

--- python/robot/robot.py
+++ python/robot/robot.py
@@ -1,22 +1,5 @@
 #!/usr/bin/env python3
 
-"""robot.py
-    
-A file argument can be used i.e.:  robot.py inputfile.txt
-Or pipe stdin:  cat input.txt | robot.py
-    
-Example input:
-UP 1
-RIGHT 10
-DOWN 5
-LEFT 2
-
-% cat input.txt | python3 robot.py
-(8,-4)    
-    
-    
-"""
-
 from robot_def import *
 
 robot()
